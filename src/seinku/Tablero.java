02857192fc94a16315b8a2bef6faf0c37fda5990
rodriantonhito <alum.Antonio.Rodriguez@ieslosremedios.org>
2015-03-12 13:50:16 +0100

    Solitario

---
+++ src/seinku/Tablero.java
@@ -0,0 +1,80 @@
+package seinku;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Tablero {
+
+    private final int FILA = 7;
+    private final int COLUMNA = 7;
+
+    private char[][] tablero;
+
+    private String[] patron;
+
+    public Tablero() {
+        tablero = new char[FILA][COLUMNA];
+
+    }
+
+    public void rellenarTablero() {
+        for (int i = 0; i < FILA; i++) {
+            for (int j = 0; j < COLUMNA; j++) {
+                if (i < 2 || i > 4) {
+                    switch (j) {
+                        case 0:
+                            tablero[i][j] = ' ';
+                            break;
+                        case 1:
+                            tablero[i][j] = ' ';
+                            break;
+                        case 2:
+                            tablero[i][j] = '#';
+                            break;
+                        case 3:
+                            tablero[i][j] = '#';
+                            break;
+                        case 4:
+                            tablero[i][j] = '#';
+                            break;
+                        case 5:
+                            tablero[i][j] = ' ';
+                            break;
+                        case 6:
+                            tablero[i][j] = ' ';
+                            break;
+                    }
+                } else {
+                    if (i == 2 || i == 4) {
+                        tablero[i][j] = '#';
+                    } else {
+                        if (j == 3) {
+                            tablero[i][j] = '.';
+                        } else {
+                            tablero[i][j] = '#';
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public String dameMovimiento(int posColumna, int posFila, int posFinalColum, int posFinalFila ){
+       char datoOrigen = tablero[posColumna][posFila];
+       char datoFinal= tablero[posFinalColum][posFinalFila];
+    
+    
+    }
+
+    public String dibujarTablero() {
+        String texto = "";
+        for (int i = 0; i < FILA; i++) {
+            for (int j = 0; j < COLUMNA; j++) {
+                texto += tablero[i][j];
+            }
+            texto += "\n";
+        }
+        return texto;
+    }
+}
